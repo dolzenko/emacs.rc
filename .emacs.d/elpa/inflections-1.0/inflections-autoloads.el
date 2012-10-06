ce2829586bf3c0806db617c395f632853907282e
Evgeniy Dolzhenko <dolzenko@gmail.com>
2012-10-06 15:46:34 +0400

    fourth

---
+++ .emacs.d/elpa/inflections-1.0/inflections-autoloads.el
@@ -0,0 +1,24 @@
+;;; inflections-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "inflections" "inflections.el" (20319 3761))
+;;; Generated autoloads from inflections.el
+
+
+
+;;;***
+
+;;;### (autoloads nil nil ("inflections-pkg.el") (20319 3761 702000))
+
+;;;***
+
+(provide 'inflections-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; inflections-autoloads.el ends here

37499cdd8dcfbb42f09a7da8f7f10bb8264aaac0
Ev Dolzhenko <evgeniy.dolzhenko@blacksquaremedia.com>
2014-01-27 17:36:23 +0400

    Update packages

---
+++ elpa/go-autocomplete-20140102.1333/go-autocomplete-pkg.el
@@ -0,0 +1,1 @@
+(define-package "go-autocomplete" "20140102.1333" "auto-complete-mode backend for go-mode" '((auto-complete "1.4.0")))
---
+++ elpa/ruby-tools-20140113.45/ruby-tools-autoloads.el
@@ -0,0 +1,27 @@
+;;; ruby-tools-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ruby-tools" "ruby-tools.el" (21207 46841 841972
+;;;;;;  688000))
+;;; Generated autoloads from ruby-tools.el
+
+(autoload 'ruby-tools-mode "ruby-tools" "\
+Collection of handy functions for ruby-mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ruby-tools-pkg.el") (21207 46841 861445
+;;;;;;  679000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ruby-tools-autoloads.el ends here

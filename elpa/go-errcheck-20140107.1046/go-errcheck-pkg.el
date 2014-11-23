601e5771398734bb6a2b33bd8c485eb6bdc64a10
Ev Dolzhenko <evgeniy.dolzhenko@blacksquaremedia.com>
2014-11-23 20:08:39 +0300

    elpa

---
+++ elpa/go-errcheck-20140107.1046/go-errcheck-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "go-errcheck" "20140107.1046" "errcheck integration for go-mode" 'nil)
---
+++ elpa/gotest-20141023.914/gotest-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gotest" "20141023.914" "Launch GO unit tests" '((s "1.9.0") (f "0.11.0") (go-mode "20131222")) :url "https://github.com/nlamirault/gotest.el" :keywords ("go" "tests"))
